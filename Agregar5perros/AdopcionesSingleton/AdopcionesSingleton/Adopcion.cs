740b0d0c02c3f0076ff34bb9c7252db1463a34c4
edgar199707 <41405538+edgar199707@users.noreply.github.com>
2019-07-03 21:43:08 -0500

    Add files via upload

---
+++ Agregar5perros/AdopcionesSingleton/AdopcionesSingleton/Adopcion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdopcionesSingleton
+{
+    class Adopcion
+    {
+        public Persona PersonaAdoptada { set; get; }
+        public Perro PerroAdoptado { get; set; }
+        public DateTime FechaAdopcion { get { return DateTime.Now; } }
+    }
+}
---
+++ Agregar5perros/AdopcionesSingleton/AdopcionesSingleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdopcionesSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Datos dato1 = Datos.GetInstancia();
+            dato1.ListaPerros.Add(new Perro() { Id = 1, Nombre = "perro #1", Edad = 7 });
+            dato1.ListaPerros.Add(new Perro() { Id = 2, Nombre = "perro #2", Edad = 7 });
+            dato1.ListaPerros.Add(new Perro() { Id = 3, Nombre = "perro #3", Edad = 7 });
+            dato1.ListaPerros.Add(new Perro() { Id = 4, Nombre = "perro #4", Edad = 7 });
+            dato1.ListaPerros.Add(new Perro() { Id = 5, Nombre = "perro #5", Edad = 7 });
+
+
+            cargarDatos();
+
+            foreach (Perro perro in dato1.ListaPerros)
+            {
+                Console.WriteLine("ID: " + perro.Id + " Nombre: " + perro.Nombre + " Edad: " + perro.Edad);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void cargarDatos()
+        {
+            Datos dato = Datos.GetInstancia();
+            dato.ListaPerros.Add(new Perro() { Id = 6, Nombre = "segundo perro #1", Edad = 7 });
+            dato.ListaPerros.Add(new Perro() { Id = 7, Nombre = "segundo perro #2", Edad = 7 });
+            dato.ListaPerros.Add(new Perro() { Id = 8, Nombre = "segundo perro #3", Edad = 7 });
+            dato.ListaPerros.Add(new Perro() { Id = 9, Nombre = "segundo perro #4", Edad = 7 });
+            dato.ListaPerros.Add(new Perro() { Id = 10, Nombre = "segundo perro #5", Edad = 7 });
+        }
+    }
+}
